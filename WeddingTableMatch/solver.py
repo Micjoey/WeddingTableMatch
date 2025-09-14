a3ce001a93fa7a1d7eec4f5cf3457125bbff37ae
Macallan S. <55409474+Micjoey@users.noreply.github.com>
2025-09-13 21:17:59 -0700

    Initialize WeddingTableMatch package

---
+++ WeddingTableMatch/solver.py
@@ -0,0 +1,13 @@
+"""Solver stub for future OR-Tools implementation."""
+
+
+class SeatingModel:
+    """A seating model that will use OR-Tools to solve table assignments."""
+
+    def build(self):
+        """Build the optimization model."""
+        raise NotImplementedError
+
+    def solve(self):
+        """Solve the optimization model."""
+        raise NotImplementedError
